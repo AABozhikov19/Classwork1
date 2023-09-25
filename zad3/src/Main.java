d1794a193b74ab38c51877b965adfd578fa56fe2
Александър Ангелов Божиков <AABozhikov19@codingburgas.bg>
2023-09-25 11:01:37 +0300

    Add third exersice

---
+++ zad3/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Person firstPerson = new Person();
+        firstPerson.setName("Alex");
+        firstPerson.setAge(18);
+
+        firstPerson.IntroduceYourself();
+    }
+}
